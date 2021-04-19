3f16f42bfcf59968d17b704cbe97d324f0415939
gouki510 <77923603+gouki510@users.noreply.github.com>
2021-04-19 17:41:54 +0900

    Add files via upload

---
+++ train_test.py
@@ -0,0 +1,92 @@
+import os
+import shutil
+import random
+
+
+def image_dir_train_test_sprit(original_dir, base_dir, train_size=0.8):
+    '''
+    画像データをトレインデータとテストデータにシャッフルして分割します。フォルダもなければ作成します。
+
+    parameter
+    ------------
+    original_dir: str
+      オリジナルデータフォルダのパス その下に各クラスのフォルダがある
+    base_dir: str
+      分けたデータを格納するフォルダのパス　そこにフォルダが作られます
+    train_size: float
+      トレインデータの割合
+    '''
+    try:
+        os.mkdir(base_dir)
+    except FileExistsError:
+        print(base_dir + "は作成済み")
+
+    #クラス分のフォルダ名の取得
+    dir_lists = os.listdir(original_dir)
+    dir_lists = [f for f in dir_lists if os.path.isdir(os.path.join(original_dir, f))]
+    original_dir_path = [os.path.join(original_dir, p) for p in dir_lists]
+
+    num_class = len(dir_lists)
+
+    # フォルダの作成(トレインとバリデーション)
+    try:
+        train_dir = os.path.join(base_dir, 'train')
+        os.mkdir(train_dir)
+    except FileExistsError:
+        print(train_dir + "は作成済み")
+
+    try:
+        validation_dir = os.path.join(base_dir, 'validation')
+        os.mkdir(validation_dir)
+    except FileExistsError:
+        print(validation_dir + "は作成済み")
+
+    #クラスフォルダの作成
+    train_dir_path_lists = []
+    val_dir_path_lists = []
+    for D in dir_lists:
+        train_class_dir_path = os.path.join(train_dir, D)
+        try:
+            os.mkdir(train_class_dir_path)
+        except FileExistsError:
+            print(train_class_dir_path + "は作成済み")
+        train_dir_path_lists += [train_class_dir_path]
+        val_class_dir_path = os.path.join(validation_dir, D)
+        try:
+            os.mkdir(val_class_dir_path)
+        except FileExistsError:
+            print(val_class_dir_path + "は作成済み")
+        val_dir_path_lists += [val_class_dir_path]
+
+
+    #元データをシャッフルしたものを上で作ったフォルダにコピーします。
+    #ファイル名を取得してシャッフル
+    for i,path in enumerate(original_dir_path):
+        files_class = os.listdir(path)
+        random.shuffle(files_class)
+        # 分割地点のインデックスを取得
+        num_bunkatu = int(len(files_class) * train_size)
+        #トレインへファイルをコピー
+        for fname in files_class[:num_bunkatu]:
+            src = os.path.join(path, fname)
+            dst = os.path.join(train_dir_path_lists[i], fname)
+            shutil.copyfile(src, dst)
+        #valへファイルをコピー
+        for fname in files_class[num_bunkatu:]:
+            src = os.path.join(path, fname)
+            dst = os.path.join(val_dir_path_lists[i], fname)
+            shutil.copyfile(src, dst)
+        print(path + "コピー完了")
+
+    print("分割終了")
+
+
+def main():
+    original_dir = "HMDB_CNN2"
+    base_dir = "sprit_data"
+    train_size = 0.8
+    image_dir_train_test_sprit(original_dir, base_dir, train_size)
+
+
+if __name__ == "__main__":
+    main()
